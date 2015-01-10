90b263db495fca5ae14c41979ec6ea3f8239b24d
Jonell <jonellalvi@gmail.com>
2015-01-09 18:53:19 -0800

    fixed scoring bug

--- quiz.py
+++ quiz.py
@@ -16,55 +16,56 @@ class Quiz:
     answers = []
 
     def __init__(self):
-      question_types = (Add, Multiply)
-      # question_types[0](1, 5)
-      # generate 10 random questions with numbers from 1 to 10
-      for _ in range(10):
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 10)
-        question = random.choice(question_types)(num1, num2)
-        # add these questions into self.questions
-        self.questions.append(question)
-
-
+        question_types = (Add, Multiply)
+        # question_types[0](1, 5)
+        # generate 10 random questions with numbers from 1 to 10
+        for _ in range(10):
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            question = random.choice(question_types)(num1, num2)
+            # add these questions into self.questions
+            self.questions.append(question)
 
 
     def take_quiz(self):
-      # log the start time
-      self.start_time = datetime.datetime.now()
-      # ask all of the questions
-      for question in self.questions:
+        # log the start time
+        self.start_time = datetime.datetime.now()
+        # ask all of the questions
+        for question in self.questions:
         # log if they got the question right
-        self.answers.append(self.ask(question))
-      else:
-        # log the end time
-        self.end_time = datetime.datetime.now()
-      return self.summary()
+            self.answers.append(self.ask(question))
+        else:
+            # log the end time
+            self.end_time = datetime.datetime.now()
+        return self.summary()
+
 
     def ask(self, question):
-      correct = False
-      # log the start time
-      question_start = datetime.datetime.now()
-      # capture the answer
-      answer = input(question.text + ' = ')
-      # check the answer
-      if answer == str(question.answer):
-        correct = True
-      # log the end time
-      question_end = datetime.datetime.now()
-      # if the answer's right, send back True
-      # otherwise, send back False
-      # send back the elapsed time, too
-      return correct, question_end - question_start
+        correct = False
+        # log the start time
+        question_start = datetime.datetime.now()
+        # capture the answer
+        # use raw_input if using Python 2.x, input if 3.x!
+        answer = raw_input(question.text + ' = ')
+        # check the answer
+        if answer == str(question.answer):
+            correct = True
+        # log the end time
+        question_end = datetime.datetime.now()
+        # if the answer's right, send back True
+        # otherwise, send back False
+        # send back the elapsed time, too
+        return correct, question_end - question_start
 
 
     def total_correct(self):
-      # return the total # of correct answers
-      total = 0
-      for answer in self.answers:
-         if answer[0]:
-            total +=1
-      return total
+        # return the total # of correct answers
+        total = 0
+        for answer in self.answers:
+            if answer[0]:
+                total += 1
+        return total
+
 
     def summary(self):
         # print how many you got right and the total # of questions. 5/10
@@ -75,5 +76,6 @@ class Quiz:
         print("It took you {} seconds total.".format(
               (self.end_time-self.start_time).seconds
           ))
+
 
 Quiz().take_quiz()
